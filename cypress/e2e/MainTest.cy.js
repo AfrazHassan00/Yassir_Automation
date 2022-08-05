cf7aa489e1a4a4c5f603925dd04e3ce8ea2673dc
Afraz Hassan <afrazhassan14@gmail.com>
2022-08-06 00:58:19 +0500

    fix(YassirAssignment): Completeting EXPLORER PAGE testcases

--- cypress/e2e/MainTest.cy.js
+++ cypress/e2e/MainTest.cy.js
@@ -35,10 +35,20 @@ describe ('TESTING OF GO CITY APPLICATION',()=>{
         cy.get(CSS.EXPLORER_PAGE).click()
         cy.get(CSS.EXPLORER_ALL_LINK).each(($a,index,$list)=>{
 
-          cy.get($list).click({multiple:true})
-            
-            
+          cy.get($list).click({multiple:true}) 
+        
         })
+    
      })
+     it('Adding explrorer card to the cart',()=>{
+        cy.get(CSS.SELECT_DROP_DOWN_EXPLORER_PAGE).select("2 Choice Pass from $52")
+        .should('have.value','Bos_Prod_Exp_c2')
+        cy.get(CSS.PLUS_SIGN).click()
+        cy.get(CSS.CHECKOUT).should('have.text','Checkout').click()
+        cy.get(CSS.CHECKOUT_PAGE_TITLE).should('have.text','Review your order')
+        cy.get(CSS.DELETE_FROM_CART_TEXT).should('have.text','Delete')
+        cy.get(CSS.DELETE_FROM_CART_ICON).click()
+        cy.get(CSS.CONFIRMATION_ALERT).should('have.text','Yes, remove').click()
 
+})
 })
--- cypress/e2e/configuration.cy.js
+++ cypress/e2e/configuration.cy.js
@@ -5,6 +5,7 @@ var CONSTANTS = {
 
 var CLASS_CSS = {
   HOME_BUTTON : "a[title='Home']",
+  HOME_LOGO : ".logo > img",
   BUY_BUTTON : "a[class='lc-font__regular']",
   BUY_NOW_BUTTON : "a[href='/boston/en-us/products/all-inclusive/pricing'",
   PASSES_PARENT_CLASS : ".lc-products-list__item-wrapper",
@@ -17,7 +18,17 @@ var CLASS_CSS = {
   EXPLORER_PAGE : "a[data-testid='menu-main-pass_product-exp']",
   ALL_INCLUSIVE_PAGE_TITLE : "a[data-testid=menu-main-pass_product-go]",
   EXPLORER_PAGE_CHECKOUT : "a[data-testid='sidecart-checkout-link']",
-  EXPLORER_ALL_LINK : ".lpg-attractions-card__details"
+  EXPLORER_ALL_LINK : ".lpg-attractions-card__details",
+  SELECT_DROP_DOWN_EXPLORER_PAGE : "select[class='form-control']",
+  PLUS_SIGN : ":nth-child(3) > .lc-cart__item-amount-wrapper > .lc-cart__item-amount > [data-testid='cartItemIncrease']",
+  ADULT_PRICE : ":nth-child(3) > .lc-cart__item-price > span > .formatted-price",
+  ORDER_PRICE : ".lc-cart__prices-number > .formatted-price",
+  CHECKOUT_PAGE_TITLE : ".content__title",
+  DELETE_FROM_CART_ICON : ".lc-cart__item-delete",
+  DELETE_FROM_CART_TEXT : "button[class ='lc-cart__item-delete']",
+  
+  CONFIRMATION_ALERT : ".custom-ui > :nth-child(2)"
+
   
 }
 
