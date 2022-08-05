45d332a8c3e5bd592bb93f7636c411680d0173d9
Afraz Hassan <afrazhassan14@gmail.com>
2022-08-05 14:35:56 +0500

    feat(YassirAssignment): first stable commit

--- cypress/e2e/MainTest.cy.js
+++ cypress/e2e/MainTest.cy.js
@@ -1,3 +1,44 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+})
+
 const CSS = require('../e2e/configuration.cy').CLASS_CSS
 const URL = require('../e2e/configuration.cy').CONSTANTS.URL
 const EXPLORER_LINKS = require('../e2e/configuration.cy').EXPLORER_PAGE_LINKS
+
+describe ('TESTING OF GO CITY APPLICATION',()=>{
+
+
+    it('Navigating to ALL INCLUSIVE PASS PAGE',()=>{
+        cy.visit(URL + "/boston/en-us/products/all-inclusive")
+        cy.get(CSS.ALL_INCLUSIVE_PAGE_TITLE).should('have.text','All-Inclusive Pass')
+
+    })
+
+    it('Redirecting user to the home page',()=>{
+        cy.get(CSS.HOME_BUTTON).click()
+        
+        
+    })
+
+    
+    it('Redirecting user to the pricing page, checking out and back to Home Page ',()=>{
+         cy.get(CSS.BUY_BUTTON).click()
+         cy.get(CSS.BUY_NOW_BUTTON).click()
+         cy.get(CSS.PASSES_PARENT_CLASS).find(CSS.PASSES_CHILD_CLASS).eq(0).should('have.text','1 day pass')
+         .click({force:true})
+         cy.get(CSS.CHECKOUT).should('have.text','Checkout').click()
+         cy.get(CSS.HOME_BUTTON).click()
+     })
+
+     it('Navigation to the EXPLORER PAGE and TESTING all EXPLORER LINKS',()=>{
+        cy.get(CSS.EXPLORER_PAGE).click()
+        cy.get(CSS.EXPLORER_ALL_LINK).each(($a,index,$list)=>{
+
+          cy.get($list).click({multiple:true})
+            
+            
+        })
+     })
+
+})
--- cypress/e2e/configuration.cy.js
+++ cypress/e2e/configuration.cy.js
@@ -4,29 +4,24 @@ var CONSTANTS = {
 }
 
 var CLASS_CSS = {
+  HOME_BUTTON : "a[title='Home']",
   BUY_BUTTON : "a[class='lc-font__regular']",
-  PASSES_CLASS : "lc-products-list__item-wrapper",
-  SELECT_BUTTON : "button[class='btn lc-products-list__item-select lc-font__regular']",
+  BUY_NOW_BUTTON : "a[href='/boston/en-us/products/all-inclusive/pricing'",
+  PASSES_PARENT_CLASS : ".lc-products-list__item-wrapper",
+  PASSES_CHILD_CLASS : ".lc-products-list__item-name",
   CART_ICON : "div[class='cart-icon__icon']",
   CART_AMOUNT : "div[class ='lc-cart__item-amount-value']",
-  CHECKOUT : "a[class='lc-cart__purchase lc-font__regular ']",
+  CHECKOUT : "a[data-testid='sidecart-checkout-link']",
   CHECKOUT_PAGE_TITLE : "h1[class='content__title']",
   CHOOSING_DESTINATION : "div[class ='banner__cta']",
-  BOSTON : "a[title='Boston']",
   EXPLORER_PAGE : "a[data-testid='menu-main-pass_product-exp']",
-  EXPLORER_PAGE_CHECKOUT : "a[data-testid='sidecart-checkout-link']"
+  ALL_INCLUSIVE_PAGE_TITLE : "a[data-testid=menu-main-pass_product-go]",
+  EXPLORER_PAGE_CHECKOUT : "a[data-testid='sidecart-checkout-link']",
+  EXPLORER_ALL_LINK : ".lpg-attractions-card__details"
   
 }
 
-var EXPLORER_PAGE_LINKS = {
-  HOW_IT_WORKS : "a[data-testid='menu-main-subpage-pass_product-how_it_works']",
-  ATTRACTIONS : "a[data-testid='menu-main-subpage-pass_product-attractions']",
-  RESERVATIONS : "a[data-testid='menu-main-subpage-pass_product-reservations']",
-  DOWNLOAD_YOUR_GUIDE : "a[data-testid='menu-main-guidebook']"
-
-}
 module.exports = {
   CONSTANTS,
-  CLASS_CSS,
-  EXPLORER_PAGE_LINKS
+  CLASS_CSS
 }
